8e987cef6bd8e642af587b5ae7dba065aaf493e2
Macailh <salvadorenriquegb@gmail.com>
2024-04-14 18:35:41 -0600

    Add env var for api gateway url

--- cdk-v2/apigateway-stages/apigateway_stages/apigateway_stages_stack.py
+++ cdk-v2/apigateway-stages/apigateway_stages/apigateway_stages_stack.py
@@ -1,9 +1,6 @@
-from aws_cdk import (
-    Stack,
-    aws_lambda as _lambda,
-    aws_apigateway as apigateway
-)
+from aws_cdk import Aws, Stack, aws_lambda as _lambda, aws_apigateway as apigateway
 from constructs import Construct
+
 
 class ApigatewayStagesStack(Stack):
 
@@ -12,15 +9,17 @@ class ApigatewayStagesStack(Stack):
 
         # Define la función Lambda que será el backend de tu API
         my_lambda = _lambda.Function(
-            self, 'MyLambda',
-            code=_lambda.Code.from_asset('apigateway_stages/lambda_code'),
-            handler='main.handler',
+            self,
+            "MyLambda",
+            code=_lambda.Code.from_asset("apigateway_stages/lambda_code"),
+            handler="main.handler",
             runtime=_lambda.Runtime.PYTHON_3_10,
         )
 
         # Crea el API Gateway sin despliegue automático
         api = apigateway.LambdaRestApi(
-            self, 'MyAPI',
+            self,
+            "MyAPI",
             handler=my_lambda,
             proxy=False,
             deploy=False,  # Evita el despliegue automático
@@ -28,36 +27,51 @@ class ApigatewayStagesStack(Stack):
 
         # Crea el primer despliegue para el stage 'dev'
         dev_deployment = apigateway.Deployment(
-            self, 'DevDeployment',
+            self,
+            "DevDeployment",
             api=api,
             # Es opcional agregar una descripción a tus despliegues
-            description='Deployment for the development stage',
+            description="Deployment for the development stage",
         )
 
         # Crea el stage 'dev' utilizando el despliegue anterior
         dev_stage = apigateway.Stage(
-            self, 'DevStage',
+            self,
+            "DevStage",
             deployment=dev_deployment,
-            stage_name='dev',
+            stage_name="dev",
         )
 
         # Opcionalmente, añade el recurso '/docs' específicamente para 'dev'
-        docs = api.root.add_resource('docs')
-        docs.add_method('GET')  # Define cómo manejarás las solicitudes GET a '/docs'
+        api.root.add_method("GET")
+        # docs = api.root.add_resource('docs')
+        # docs.add_method('GET')  # Define cómo manejarás las solicitudes GET a '/docs'
 
         # Crea el segundo despliegue para el stage 'prod'
         prod_deployment = apigateway.Deployment(
-            self, 'ProdDeployment',
+            self,
+            "ProdDeployment",
             api=api,
-            description='Deployment for the production stage',
+            description="Deployment for the production stage",
         )
 
         # Crea el stage 'prod' utilizando el despliegue anterior
         prod_stage = apigateway.Stage(
-            self, 'ProdStage',
+            self,
+            "ProdStage",
             deployment=prod_deployment,
-            stage_name='prod',
+            stage_name="prod",
         )
 
         # Asigna uno de los stages como predeterminado, si es necesario
-        api.deployment_stage = dev_stage
+        # api.deployment_stage = dev_stage
+
+        # Aquí es donde construimos la URL manualmente
+        region = Aws.REGION
+        api_id = api.rest_api_id
+        stage = (
+            "prod"  # Asume que usas el stage predeterminado que crea `LambdaRestApi`
+        )
+
+        api_url = f"https://{api_id}.execute-api.{region}.amazonaws.com/{stage}"
+        my_lambda.add_environment("API_URL", api_url)
